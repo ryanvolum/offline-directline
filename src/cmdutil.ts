a45c100ad9942f34af33d5e9676bb2c4776a6690
Ryan <ryanvolum@gmail.com>
2018-09-05 15:50:15 -0700

    renamed client to directline, added handling for when both endpoints aren't passed in

--- src/cmdutil.ts
+++ src/cmdutil.ts
@@ -4,10 +4,14 @@ import * as express from 'express';
 import * as program from 'commander';
 
 program
-    .option('-c, --client <client>', 'The endpoint/port where your client lives (e.g. "http://127.0.0.1:3000")')
+    .option('-d, --directline <directline>', 'The endpoint/port where offline-directline will run (e.g. "http://127.0.0.1:3000")')
     .option('-b, --bot <bot>', 'The endpoint/port where your bot lives (e.g. "http://127.0.0.1:3978/api/messages")')
     .action(() => {
-        const app = express();
-        directline.initializeRoutes(app, program.client, program.bot);
+        if (program.directline && program.bot) {
+            const app = express();
+            directline.initializeRoutes(app, program.directline, program.bot);
+        } else {
+            console.log('offline-directline requires you to pass the endpoint where it will be hosted on and the endpoint where your bot lives (e.g. "directline -d http://127.0.0.1:3000 -b http://127.0.0.1:3978/api/messages")');
+        }
     })
     .parse(process.argv);
